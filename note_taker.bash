4f0b045ef24c9828cefb04470bff89ea1e9259a2
John Coleman <jcoleman@pagerduty.com>
2018-10-16 22:32:45 -0700

    added better arg check, backup option of -b & removed -i interactive to prepare for less command

--- note_taker.bash
+++ note_taker.bash
@@ -2,44 +2,43 @@
 #
 # note taker executable
 
-NOTE_PAD_PATH="/$HOME/.notes_for_note_taker.txt"
+NOTE_PAD_PATH="$HOME/.notes_for_note_taker.txt"
 
 # editor set this here or through git
 # $ git config --global core.editor "emacs"
-EDITOR=$(git config core.editor 2> /dev/null || echo 'vim')
+NOTE_EDITOR="$(git config core.editor 2> /dev/null || echo 'vim')"
+USAGE_STRING="Usage: $ note [bpeft] [NS] [<SUBJECT>] [<MESSAGE_LINE_1>] [<MESSAGE_LINE_2>] ..."
 
 #######################################
 # Handles options
 # Globals:
 #   NOTE_PAD_PATH
-#   EDITOR
+#   NOTE_EDITOR
 # Arguments:
-#   -p: paste from pbpaste
-#   -i: interactive option, adds timestamp
-#   -e: edit file, does not add timestamp
-#   -t N: output $ tail -n (optional N number of lines)
+#   see README for arguments
 # Returns:
 #   None
 #######################################
 handle_options () {
     case "$1" in
+        "-b")
+            # -b to backup your NOTE_PAD_PATH
+            BACKUP_EXTENSION="$(uuidgen 2> /dev/null || echo 'backup')"
+            BACKUP_PATH="$NOTE_PAD_PATH.$BACKUP_EXTENSION"
+            cp -Rf "$NOTE_PAD_PATH" "$BACKUP_PATH"
+            echo "notes backed up to $BACKUP_PATH"
+            ;;
         "-p")
             # -p for paste
             printf "\n" >> "$NOTE_PAD_PATH"
             date >> "$NOTE_PAD_PATH"
             echo -n "(clipboard)--: $(pbpaste)" >> "$NOTE_PAD_PATH"
             printf "\n" >> "$NOTE_PAD_PATH"
-            ;;
-        "-i")
-            # -i for interactive
-            printf "\n" >> "$NOTE_PAD_PATH"
-            date >> "$NOTE_PAD_PATH"
-            echo -n "----: " >> "$NOTE_PAD_PATH"
-            "$EDITOR" "$NOTE_PAD_PATH"
+            echo "clipboard added to notes"
             ;;
         "-e")
             # -e for edit
-            "$EDITOR" "$NOTE_PAD_PATH"
+            "$NOTE_EDITOR" "$NOTE_PAD_PATH"
             ;;
         "-f")
             # -f S for find using grep with string match for S
@@ -59,7 +58,8 @@ handle_options () {
             echo ""
             ;;
         *)
-            return
+            echo "$1 looks like an argument, please use a valid argument"
+            echo "$USAGE_STRING"
             ;;
     esac
     [[ "$0" = "$BASH_SOURCE" ]] && exit 0 || return 0
@@ -77,10 +77,10 @@ handle_options () {
 #######################################
 process_notes () {
     argv_one_length=${#1}
-    if [[ "$argv_one_length" == 2 ]]; then
+    if [[ "$argv_one_length" == 2 && "$1" == -* ]]; then
         handle_options "$@"
     elif [[ "$argv_one_length" == 0 ]]; then
-        echo "Usage: $ note [pieft] [NS] [<SUBJECT>] [<MESSAGE_LINE_1>] [<MESSAGE_LINE_2>] ..."
+        echo "$USAGE_STRING"
         [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
     fi
     date >> "$NOTE_PAD_PATH"
