bc431c0e9df8c162a220b1a2739ee18217fa663b
John Coleman <jcoleman@pagerduty.com>
2018-10-20 14:13:44 -0700

    fixed undo bug on mac and organized options to be alphabetical

--- note_taker.bash
+++ note_taker.bash
@@ -7,7 +7,7 @@ NOTE_PAD_PATH="$HOME/.notes_for_note_taker.txt"
 # editor set this here or through git
 # $ git config --global core.editor "emacs"
 NOTE_EDITOR="$(git config core.editor 2> /dev/null || echo 'vim')"
-USAGE_STRING="Usage: $ note [bpeft] [NS] [<SUBJECT>] [<MESSAGE_LINE_1>] [<MESSAGE_LINE_2>] ..."
+USAGE_STRING="Usage: $ note [abcefptu] [NS] [<SUBJECT>] [<MESSAGE_LINE_1>] [<MESSAGE_LINE_2>] ..."
 
 #######################################
 # Handles options
@@ -21,6 +21,10 @@ USAGE_STRING="Usage: $ note [bpeft] [NS] [<SUBJECT>] [<MESSAGE_LINE_1>] [<MESSAG
 #######################################
 handle_options () {
     case "$1" in
+        "-a")
+            # -a for all: pipes the whole notes file through less for quick, easy viewing
+            cat "$NOTE_PAD_PATH" | less
+            ;;
         "-b")
             # -b to backup your NOTE_PAD_PATH
             BACKUP_EXTENSION="$(uuidgen 2> /dev/null || echo 'backup')"
@@ -28,13 +32,32 @@ handle_options () {
             cp -Rf "$NOTE_PAD_PATH" "$BACKUP_PATH"
             echo "notes backed up to $BACKUP_PATH"
             ;;
-        "-p")
-            # -p for paste
-            printf "\n" >> "$NOTE_PAD_PATH"
-            date >> "$NOTE_PAD_PATH"
-            echo -n "(clipboard)--: $(pbpaste)" >> "$NOTE_PAD_PATH"
-            printf "\n" >> "$NOTE_PAD_PATH"
-            echo "clipboard added to notes"
+        "-c")
+            # -c for 'clear notes'
+            read -p "Clear all notes [y/n]? " -n 1 -r CLEAR_REPLY
+            echo ""
+            # exit if not confirmed
+            if [[ ! "$CLEAR_REPLY" =~ ^[Yy]$ ]]; then
+                echo "Cancelled."
+            else
+                # create backup just in case
+                note -b
+                # clear notes file by overwriting it with blankness:
+                > "$NOTE_PAD_PATH"
+                # report success
+                echo "Cleared."
+            fi
+            read -p "Clear all backup notes too [y/n]? " -n 1 -r CLEAR_REPLY_TWO
+            echo ""
+            # exit if not confirmed
+            if [[ ! "$CLEAR_REPLY_TWO" =~ ^[Yy]$ ]]; then
+                echo "cleared with backup."
+            else
+                ls -1 "$NOTE_PAD_PATH"* | xargs rm -rf
+                # report success
+                > "$NOTE_PAD_PATH"
+                echo "cleared all notes."
+            fi
             ;;
         "-e")
             # -e for edit
@@ -48,74 +71,62 @@ handle_options () {
                 cat "$NOTE_PAD_PATH" | grep --ignore-case --before-context=3 --after-context=3 --color --extended-regexp "$2"
             fi
             ;;
+        "-p")
+            # -p for paste
+            printf "\n" >> "$NOTE_PAD_PATH"
+            date >> "$NOTE_PAD_PATH"
+            echo -n "(clipboard)--: $(pbpaste)" >> "$NOTE_PAD_PATH"
+            printf "\n" >> "$NOTE_PAD_PATH"
+            echo "clipboard added to notes"
+            ;;
         "-t")
             # -t N for tail -n N (optional)
             if [[ -z "$2" ]]; then
                 tail "$NOTE_PAD_PATH"
             else
-                tail -n $2 "$NOTE_PAD_PATH"
+                tail -n "$2" "$NOTE_PAD_PATH"
             fi
             echo ""
             ;;
-	"-a")
-	    # -a for all: pipes the whole notes file through less for quick, easy viewing
-	    cat "$NOTE_PAD_PATH" | less
-	    ;;
-	"-c")
-	    # -c for 'clear notes'
-	    # confirm user intention
-	    read -p "Clear all notes [y/n]? " -n 1 -r CLEAR_REPLY
-	    echo ""
-            # exit if not confirmed
-	    if [[ ! "$CLEAR_REPLY" =~ ^[Yy]$ ]]; then
-	    	echo "Cancelled."
-	    else 
-            	# clear notes file by overwriting it with blankness:
-		> "$NOTE_PAD_PATH"
-		# report success
-		echo "Cleared."
-	    fi
-            echo ""
-	    ;;
-	"-u")
-	    # -u for 'undo': deletes the most recent note. Specifically, it deletes lines starting
+        "-u")
+            # -u for 'undo': deletes the most recent note. Specifically, it deletes lines starting
             # with the next-to-last line and moving upwards through the file, until the next blank
             # line it encounters.
-	    # confirm user intention
-	    read -p "Clear most recent note [y/n]? " -n 1 -r CLEAR_REPLY
-	    echo ""
+            read -p "Undo most recent note [y/n]? " -n 1 -r CLEAR_REPLY
+            echo ""
             # exit if not confirmed
-	    if [[ ! "$CLEAR_REPLY" =~ ^[Yy]$ ]]; then
-	    	echo "Cancelled."
-	    else 
-        	# get the total number of lines in the notes file
-		LINES=`cat "$NOTE_PAD_PATH" | wc -l`
-		# get the penultimate line, as the last line is always blank
-		((LINES--))
-		# get the text of the penultimate line and assign it to LINE_TEXT
-		LINE_NUMBER=$LINES
-		# get the text of the penultimate line and assign it to LINE_TEXT
-		LINE_TEXT=`tail -n +"$LINE_NUMBER" "$NOTE_PAD_PATH" | head -n 1`
-		# as long as LINE_TEXT is not empty and LINE_NUMBER is not 0
-		until [ -z "$LINE_TEXT" ] || [ "$LINE_NUMBER" -eq 0 ]; do
-			# delete the last line of the notes file by copying a shortened version
-			# to a temp file and then overwriting the original with the temp
-			head -n -1 "$NOTE_PAD_PATH" > temp.txt
-			mv temp.txt "$NOTE_PAD_PATH"
-			# decrement the current variable for the current line number
-			((LINE_NUMBER--))
-			# get the text of the new last line
-			LINE_TEXT=`tail -n +"$LINE_NUMBER" "$NOTE_PAD_PATH" | head -n 1`
-		done
-		# outside of the loop we need to delete one more line to complete the clear process
-		head -n -1 "$NOTE_PAD_PATH" > temp.txt
-		mv temp.txt "$NOTE_PAD_PATH"
-		# report success
-		echo "Cleared."
-	    fi
- 	    echo ""
-	    ;;
-	*)
+            if [[ ! "$CLEAR_REPLY" =~ ^[Yy]$ ]]; then
+                echo "Cancelled."
+            else 
+                # get the total number of lines in the notes file
+                LINES="$(cat $NOTE_PAD_PATH | wc -l)"
+                # get the penultimate line, as the last line is always blank
+                ((LINES--))
+                # get the text of the penultimate line and assign it to LINE_TEXT
+                LINE_NUMBER=$LINES
+                # get the text of the penultimate line and assign it to LINE_TEXT
+                LINE_TEXT="$(tail -n +$LINE_NUMBER $NOTE_PAD_PATH | head -n 1)"
+                # as long as LINE_TEXT is not empty and LINE_NUMBER is not 0
+                until [ -z "$LINE_TEXT" ] || [ "$LINE_NUMBER" -eq 0 ]; do
+                    echo "... removing $LINE_TEXT"
+                    # delete the last line of the notes file by copying a shortened version
+                    # to a temp file and then overwriting the original with the temp
+                    cat "$NOTE_PAD_PATH" | tail -r | tail -n +2 | tail -r > temp.txt
+                    mv temp.txt "$NOTE_PAD_PATH"
+                    # decrement the current variable for the current line number
+                    ((LINE_NUMBER--))
+                    # get the text of the new last line
+                    LINE_TEXT="$(tail -n +$LINE_NUMBER $NOTE_PAD_PATH | head -n 1)"
+                done
+                # outside of the loop we need to delete one more line to complete the clear process
+                echo "... removing $LINE_TEXT"
+                cat "$NOTE_PAD_PATH" | tail -r | tail -n +2 | tail -r > temp.txt
+                mv temp.txt "$NOTE_PAD_PATH"
+                # report success
+                echo "... success"
+            fi
+            ;;
+        *)
             echo "$1 looks like an argument, please use a valid argument"
             echo "$USAGE_STRING"
             ;;
